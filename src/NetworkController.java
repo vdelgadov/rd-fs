07245939ca4db4a6539d6c255206e3b9ac4b6f3e
victor.delgadov <victor.delgadov@gmail.com>
2011-03-20 01:21:08 +0000

    Updating Network Controller and BroadCastListener. Use of Log

--- src/NetworkController.java
+++ src/NetworkController.java
@@ -5,9 +5,12 @@ public class NetworkController {
 
 	FileSystemController FSC = new FileSystemController();
 	DirectoryController DC = new DirectoryController();
-	private LinkedList<DatagramPacket> BroadcastPacketsQueue = new LinkedList<DatagramPacket>();
-
+	
 	static NetworkController nc;
+	
+	
+	//ThreadControll
+	public boolean runListener = true; 
 	
 	private NetworkController() {
 		// TODO Auto-generated constructor stub
@@ -24,27 +27,23 @@ public class NetworkController {
 	
 	public void startListener()
 	{
+		Log.me(this, "Starting Broadcast Listener");
+		nc.runListener = true;
 		BroadcastListener bl = new BroadcastListener(getInstance());
 		new Thread(bl).start();
 	}
-	public synchronized void queueDatagramPacket(DatagramPacket dp)
+	public void stopListener()
 	{
-		BroadcastPacketsQueue.addLast(dp);
+		Log.me(this, "Stoping Broadcast Listener");
+		nc.runListener = false;
 	}
-	public synchronized DatagramPacket dequeueDatagramPacket()
-	{
-		return BroadcastPacketsQueue.removeFirst();
-	}
+
 	
-	public void processPacket()
+	public synchronized void processPacket(DatagramPacket dp)
 	{
-		DatagramPacket dp = this.dequeueDatagramPacket();
 		String message = new String(dp.getData(),0,dp.getLength());
 		String[] split = message.split("@", 3);
-		if (split[0].compareTo("CD") == 0)
-		{
-			
-		}
+		Log.me(this, "Proscessing packet: " + message);
 	}
 
 }
