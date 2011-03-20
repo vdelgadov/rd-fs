9ef9fe9f0fc0707e5b9a00e0923c8586a4bff1ea
victor.delgadov <victor.delgadov@gmail.com>
2011-03-20 02:19:45 +0000

    UUID integration and Thread synchronization

--- src/BroadcastListener.java
+++ src/BroadcastListener.java
@@ -2,22 +2,20 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
-import java.net.SocketException;
-
 
 public class BroadcastListener implements Runnable {
 	NetworkController nc;
-	
+
 	public BroadcastListener(NetworkController nc)
 	{
 		this.nc = nc;
 	}
-	
-	
+
+
 	@Override
 	public void run() {
 		try {
-			InetAddress ia = InetAddress.getByName("224.0.0.22");
+			InetAddress ia = InetAddress.getByName("224.0.0.205");
 			byte[] buffer = new byte[65535];
 			int port = 4572;
 
@@ -26,22 +24,24 @@ public class BroadcastListener implements Runnable {
 			MulticastSocket ms = new MulticastSocket(port);
 			ms.joinGroup(ia);
 			while (nc.runListener) {
-				ms.receive(dp);
-				this.nc.processPacket(dp);
+				try {
+					ms.setSoTimeout(5000);
+					ms.receive(dp);
+					Log.me(this, "Received Packet: " + new String(dp.getData(),0,dp.getLength()));
+					this.nc.processPacket(dp);
+				}
+				catch(Exception e)
+				{
+					//Log.me(this,e.getMessage());
+				}
 
-				
-				Log.me(this, "Received Packet: " + new String(dp.getData(),0,dp.getLength()));
 			}
-			
-		}
-		
-		catch (SocketException se) {
-			Log.me(this,se.getMessage());
+
 		}
 		catch (IOException ie) {
 			Log.me(this,ie.getMessage());
 		}
-		
+
 	}
-	
+
 }
--- src/rdfs.java
+++ src/rdfs.java
@@ -1,18 +1,84 @@
-import java.net.InetAddress;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.UUID;
+
 
 public class rdfs {
 
 	/**
 	 * @param args
 	 */
+	static UUID uuid = null;
 	public static void main(String[] args) {
 		
 		Log.init(Log.Priority.DEBUG);
 		Log.me(null, "RDFS starting...");
 		
+		
+		Log.me(null, "Obtaining UUID");
+		getUUID();
+		
 		NetworkController nc = NetworkController.getInstance();
 		nc.startListener();
 		
+		
+		//this sleep is for debugging purposes - eliminate on final versions
+		/*
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		nc.stopListener();
+		Log.me(null, "RDFS terminating...");
+		*/
+		
+	}
+	public static void getUUID()
+	{
+		File f = new File("uuid");
+		if (f.exists())
+		{
+			FileInputStream fstream;
+			try {
+				fstream = new FileInputStream("uuid");
+			    DataInputStream in = new DataInputStream(fstream);
+			    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+				uuid = UUID.fromString(br.readLine());
+				Log.me(null, "Using existent UUID: " + uuid.toString());
+			}
+			catch (IOException e) {
+				Log.me(null,e.getMessage());
+				return;
+			}
+		}
+		else
+		{
+			uuid = UUID.randomUUID();
+			try
+			{
+				FileWriter fstream;
+				BufferedWriter out;
+				
+				fstream = new FileWriter(f);
+				out = new BufferedWriter(fstream);
+				out.write(uuid.toString());
+			    out.close();
+			    fstream.close();
+		    }
+			catch (Exception e)
+			{//Catch exception if any
+				System.err.println("Error: " + e.getMessage());
+		    }
+			
+			Log.me(null, "Generated UUID: " + uuid.toString());
+		}
 	}
 
 }
