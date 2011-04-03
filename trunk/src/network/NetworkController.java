7bdef09e6303168e4dfe7923b092109f538fc352
victor.delgadov@gmail.com <victor.delgadov@gmail.com@a6a1c369-4107-f578-ce34-0e124d47ffdf>
2011-04-03 03:04:37 +0000

    Packet processing, netowrk

--- trunk/src/network/NetworkController.java
+++ trunk/src/network/NetworkController.java
@@ -3,6 +3,8 @@ package network;
 import java.net.DatagramPacket;
 import java.util.ArrayList;
 import java.util.UUID;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
 
 import common.Log;
 import fileSystem.FileSystemController;
@@ -14,6 +16,7 @@ public class NetworkController {
 
 	FileSystemController FSC = FileSystemController.getInstance("");
 	DirectoryController DC = new DirectoryController();
+	
 	
 	static NetworkController nc;
 	
@@ -57,6 +60,41 @@ public class NetworkController {
 			Node n = new Node(UUID.fromString(split[1]));
 			this.DC.getNodeDirectory().addNode(n);
 		}
+		else if(split[0].equals("pSave"))
+		{
+			
+		}
+		//receiving file to be saved
+		//TODO vic this methods shouldnt be here?? only broadcast?
+		if(split[0].equals("send"))
+		{
+			
+			String[] sendSplit = split[1].split("@", 5);
+			String name = sendSplit[0];
+			int chunk = Integer.parseInt(sendSplit[1]);
+			long crc = Long.parseLong(sendSplit[2]);
+			int numBytes = Integer.parseInt(sendSplit[3]);
+			byte[] bytes = sendSplit[4].getBytes();
+			
+			Checksum checksum = new CRC32();
+			checksum.update(bytes,0,bytes.length);
+			long lngChecksum = checksum.getValue();
+			if(crc == lngChecksum)
+			{
+				//TODO pablo save file (FileSystemController)
+			}
+			else
+			{
+				//TODO vic send response (failed)
+				
+			}
+		}
+		//Petition to send file
+		if(split[0].equals("get"))
+		{
+			
+		}
+		
 	}
 
 }
