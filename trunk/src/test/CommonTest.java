cbf989ea47eaca55bddc931c5f4f99f35f2024f9
jpabluz@gmail.com <jpabluz@gmail.com@a6a1c369-4107-f578-ce34-0e124d47ffdf>
2011-04-03 20:55:58 +0000

    - Added CommonTest, which tests the common package. - Added Mockito library, which will be used to mock the network functionality.

---
+++ trunk/src/test/CommonTest.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.UUID;
+
+import org.junit.Test;
+
+
+/**
+ * @author Pavooou
+ *
+ */
+public class CommonTest 
+{
+	
+	@Test
+	public void getExistingUUID()
+	{
+		UUID uuid = null;
+		uuid = common.rdfs.getUUID();
+		assertNotNull(uuid);
+	}
+	
+	@Test
+	public void generateNewUUID()
+	{
+		UUID uuid = null;
+		uuid = common.rdfs.getUUID();
+		assertNotNull(uuid);
+		
+		File f = new File(common.rdfs.UUID_FILENAME);
+		
+		assertTrue(f.delete());
+		
+		UUID newUuid = null;
+		newUuid = common.rdfs.getUUID();
+		assertNotNull(newUuid);
+		assertFalse(newUuid.equals(uuid));
+	}
+	
+	@Test
+	public void readProperties() throws Exception
+	{
+		assertTrue(common.RDFSProperties.getServerPort() > -1);
+	}
+	
+}
