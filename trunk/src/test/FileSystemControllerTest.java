9f34bd8ef393deddc50bdd6db278a0765442681f
jpabluz@gmail.com <jpabluz@gmail.com@a6a1c369-4107-f578-ce34-0e124d47ffdf>
2011-04-03 01:54:30 +0000

    - Added JUnit jar and classpath to the project.

---
+++ trunk/src/test/FileSystemControllerTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FileSystemControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	@Test 
+	public void saveNewOnLocal()
+	{
+		assertTrue(true);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
