facf9e2d12fa46a2b4434b41d8cca4240ba2b71b
victor.delgadov@gmail.com <victor.delgadov@gmail.com@a6a1c369-4107-f578-ce34-0e124d47ffdf>
2011-04-26 23:39:11 +0000

    Broadcast Listener changes

--- trunk/src/network/BroadcastListener.java
+++ trunk/src/network/BroadcastListener.java
@@ -4,12 +4,17 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.Callable;
 
 import common.Log;
 import common.RDFSProperties;
 
 public class BroadcastListener implements Runnable {
 	NetworkController nc;
+	//private final ExecutorService pool;
 
 	public BroadcastListener(NetworkController nc)
 	{
@@ -21,24 +26,33 @@ public class BroadcastListener implements Runnable {
 	public void run() {
 		try {
 			InetAddress ia = InetAddress.getByName("224.0.0.205");
-			byte[] buffer = new byte[65535];
 
-			DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
 
-			MulticastSocket ms = new MulticastSocket(RDFSProperties.getBroadcastPort());
+			final MulticastSocket ms = new MulticastSocket(RDFSProperties.getBroadcastPort());
 			ms.joinGroup(ia);
+			ExecutorService executor = Executors.newFixedThreadPool(20);
 			while (nc.runListener) {
-				try {
-					ms.setSoTimeout(5000);
-					ms.receive(dp);
-					Log.me(this, "Received Packet: " + new String(dp.getData(),0,dp.getLength()));
-					//TODO is it necessary to copy the object? add to a Thread?
-					this.nc.processPacket(dp);
-				}
-				catch(Exception e)
-				{
-					//Log.me(this,e.getMessage());
-				}
+
+
+					FutureTask<String> future = 
+						new FutureTask<String>(new Callable<String>() { 
+							public String call() {
+								try {
+									byte[] buffer = new byte[65535];
+									DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
+									ms.setSoTimeout(5000);
+									ms.receive(dp);
+									Log.me(this, "Received Packet: " + new String(dp.getData(),0,dp.getLength()));
+									nc.processPacket(dp);
+								}
+								catch(Exception e)
+								{
+									Log.me(this,e.getMessage());
+								}
+								return "hi";
+							}});
+					executor.execute(future);
+
 
 			}
 
